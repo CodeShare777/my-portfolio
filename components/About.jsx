c0efc92236e1c6c1dcef0d1a026f2068061d9dcf
Test <tech@gmail.com>
2022-11-06 16:14:15 +0100

    Fixing apostrophe error.

--- components/About.jsx
+++ components/About.jsx
@@ -19,13 +19,13 @@ const About = () => {
             Kubernetes to deploy and manage microservices on the cloud.
           </p>
           <p className='py-2 text-gray-600'>
-            I started development in 2008, after graduating. During my Master's
-            studies my love for programming started to get more intense, when I
-            developed 4G simulators using C++ and Python. Then I explored
-            multiple different technologies, like Qt, C# and Cuda. As Web
-            Frameworks became more powerful, I started developing Web apps using
-            Django and Node.js. Then I worked with React for some time and now
-            exploring Next.js.
+            I started development in 2008, after graduating. During my
+            Master&apos;s studies my love for programming started to get more
+            intense, when I developed 4G simulators using C++ and Python. Then I
+            explored multiple different technologies, like Qt, C# and Cuda. As
+            Web Frameworks became more powerful, I started developing Web apps
+            using Django and Node.js. Then I worked with React for some time and
+            now exploring Next.js.
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
