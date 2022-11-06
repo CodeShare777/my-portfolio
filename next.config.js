5a9dc3696177aa1f2db75ce35c92e7a9f2a7eaa2
Test <tech@gmail.com>
2022-11-06 13:16:26 +0100

    Fixes for making SSG work.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
